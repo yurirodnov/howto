4ab63c8abf67579a01171ba01009466297da689e
yuri_rodnov <somewaystodo@yandex.ru>
2024-12-25 21:55:46 +0300

    Add Chips code

--- UI/Chips/script.js
+++ UI/Chips/script.js
@@ -1,1 +1,29 @@
-window.onload = () => {};
+window.onload = () => {
+  const chipsCloud = document.querySelector("#chips-cloud");
+  const input = document.querySelector("#input");
+  const button = document.querySelector("#button");
+
+  const handleChipRemoval = (e) => {
+    const allChips = Array.from(document.getElementsByClassName("chip"));
+    for (const chip of allChips) {
+      if (e.target === chip) {
+        chipsCloud.removeChild(chip);
+      }
+    }
+  };
+
+  const handleButtonClick = () => {
+    const inputValue = input.value;
+    if (inputValue.length < 1) {
+      return;
+    } else {
+      const chip = document.createElement("div");
+      chip.classList.add("chip");
+      chip.textContent = inputValue;
+      chip.addEventListener("click", handleChipRemoval);
+      chipsCloud.appendChild(chip);
+    }
+  };
+
+  button.addEventListener("click", handleButtonClick);
+};
